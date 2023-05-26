bf6c11172e459471b5fcb8119fd07d163f946045
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-26 16:16:45 +0900

    FEAT: Container 생성

---
+++ src/styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const SearchContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 500px;
+  background-image: linear-gradient(
+    45deg,
+    rgba(226, 254, 12, 0.5),
+    rgba(4, 254, 0, 0.6)
+  );
+`;
+
+export const ListContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 70px;
+`;
