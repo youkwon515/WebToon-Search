de06337e3cceb67871d8530ae5fabd6c8f46956a
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-30 11:31:36 +0900

    FEAT: 검색 결과 검사

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,20 +1,31 @@
 import ListItem from "../ListItem";
 import * as S from "./styled";
-function List({ webtoon }) {
+function List({ webtoon, InputValue, detailValue }) {
   return (
     <S.List>
-      {webtoon.map(({ _id, title, author, url, img, service }) => {
-        return (
-          <ListItem
-            key={_id}
-            title={title}
-            author={author}
-            url={url}
-            img={img}
-            service={service}
-          />
-        );
-      })}
+      {webtoon.length !== 0 ? (
+        webtoon.map(({ _id, title, author, url, img, service }) => {
+          const handleDetailValue = () => {
+            const detailArr = { id: _id, title, author, url, img, service };
+            detailValue(detailArr);
+          };
+          return (
+            <ListItem
+              key={_id}
+              title={title}
+              author={author}
+              url={url}
+              img={img}
+              service={service}
+              onClick={handleDetailValue}
+            />
+          );
+        })
+      ) : InputValue === undefined ? (
+        <h1>제목이나 작가를 검색하세요.</h1>
+      ) : (
+        <h1>검색 결과가 없습니다.</h1>
+      )}
     </S.List>
   );
 }
