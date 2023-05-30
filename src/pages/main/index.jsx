49e06df5f764a74066545cb0defe41f750fc98c2
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-30 14:20:40 +0900

    FEAT: loading state 생성

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import Input from "../../components/Input";
 import List from "../../components/List";
+import Loading from "../../components/Loding";
 import * as S from "./styled";
 
 function Main({ detailValue }) {
+  const [loading, setLoading] = useState(false);
   const [webtoon, setWebtoon] = useState([]);
   const [InputValue, setInputValue] = useState(undefined);
   async function webToonSearch(value) {
+    setLoading(true);
     const res = await fetch(
       `https://korea-webtoon-api.herokuapp.com/search?keyword=${value}`
     );
     const data = await res.json();
     setWebtoon(data.webtoons);
+    setLoading(false);
   }
 
   const onSearch = (e) => {
@@ -28,6 +32,7 @@ function Main({ detailValue }) {
 
   return (
     <>
+      {loading ? <Loading /> : null}
       <S.SearchContainer>
         <Input onKeyUp={onSearch} placeholder="제목이나 작가를 검색하세요." />
       </S.SearchContainer>
