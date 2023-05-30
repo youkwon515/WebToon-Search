48eba3ce1b68a8e07bf153aa8c820f634f2a880f
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-30 11:32:00 +0900

    FEAT: Link로 연결

--- src/components/ListItem/index.jsx
+++ src/components/ListItem/index.jsx
@@ -1,17 +1,19 @@
 import * as S from "./styled";
-
-function ListItem({ title, author, url, img, service }) {
+import { Link } from "react-router-dom";
+function ListItem({ title, author, url, img, service, onClick }) {
   return (
-    <S.Item>
-      <S.Img src={img} />
-      <S.ContentsBox>
-        <S.TitleBox>
-          <S.Title>{title}</S.Title>
-        </S.TitleBox>
-        <S.author>{author}</S.author>
-        <S.serviceBox>{service}</S.serviceBox>
-      </S.ContentsBox>
-    </S.Item>
+    <Link to="/detail" onClick={onClick}>
+      <S.Item>
+        <S.Img src={img} />
+        <S.ContentsBox>
+          <S.TitleBox>
+            <S.Title>{title}</S.Title>
+          </S.TitleBox>
+          <S.author>{author}</S.author>
+          <S.serviceBox>{service}</S.serviceBox>
+        </S.ContentsBox>
+      </S.Item>
+    </Link>
   );
 }
 
