0fdeb12654d2bf389490d374092e9dec6aef457c
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-05-30 14:20:08 +0900

    FEAT: 로딩 디자인

---
+++ src/components/Loding/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const LoadingBox = styled.div`
+  position: absolute;
+  z-index: 999;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
